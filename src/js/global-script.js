90acfc0b6c7fd563797a07cb552ac16070a9d11e
amstelart <amsteldroid@gmail.com>
2019-10-10 17:42:13 +0300

    dev-01

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,22 +1,34 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+  // https://github.com/digitalBush/jquery.maskedinput
+  $(".phone-mask").mask("+7(999) 999-9999");
 
-// Изоляция без jQuery
-// (function(){
-//   // code
-// }());
+  // https://www.jqueryscript.net/time-clock/psg-countdown-timer.html
+  var timer = new PsgTimer({
+      selector: '#zefricaTimer',
+      currentDateTime: Date.UTC(2018, 0, 26, 12, 0, 0),
+      endDateTime: 'UTC+02:00 26.02.2018 13:00:00',
+      multilpeBlocks: true,
+      animation: 'fade',
+      labels: {
+          days: 'Дней',
+          hours: 'Часов',
+          minutes: 'Минут',
+          seconds: 'Секунд'
+      },
+      callbacks: {
+          onInit: function () {
+              console.log('Hello world!');
+          }
+      }
+  });
 
-// На проекте нет jQuery, но хочется $( document ).ready...
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   // code
-// });
+  $(".adv-slider").owlCarousel({
+    items: 2,
+    nav: true,
+    dots: false,
+    loop: true,
+    margin: 15,
+    center: true
+  });
+});
