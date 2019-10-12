c5db24d98ac51b459ad457257301534066de2318
amstelart <amsteldroid@gmail.com>
2019-10-12 05:35:16 +0300

    dev-2

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,5 +1,50 @@
 // Если на проекте jQuery
 $( document ).ready(function() {
+
+  var mainNav = $('.navbar'),
+      contentSections = $('.scroll-section');
+
+  $(window).on('scroll', function(){
+
+		//on desktop - update the active link in the secondary fixed navigation
+		updatemainNavigation();
+	});
+
+  function updatemainNavigation() {
+		contentSections.each(function(){
+			var actual = $(this),
+				actualHeight = actual.height() + parseInt(actual.css('paddingTop').replace('px', '')) + parseInt(actual.css('paddingBottom').replace('px', '')),
+				actualAnchor = mainNav.find('a[href="#'+actual.attr('id')+'"]');
+			if ( ( actual.offset().top - mainNav.height() <= $(window).scrollTop() ) && ( actual.offset().top +  actualHeight - mainNav.height() > $(window).scrollTop() ) ) {
+				actualAnchor.addClass('active');
+			}else {
+				actualAnchor.removeClass('active');
+			}
+		});
+	}
+
+  //smooth scrolling when clicking on the secondary navigation items
+	mainNav.find('ul a').on('click', function(event){
+      event.preventDefault();
+      var target= $(this.hash);
+      $('body,html').animate({
+      	'scrollTop': target.offset().top + 1
+      	}, 400
+      );
+      //on mobile - close secondary navigation
+      $('.navbar-toggler').addClass('collapsed');
+      mainNav.find('.navbar-collapse').removeClass('show');
+  });
+
+  $('.scroll-link').on('click', function(event){
+    event.preventDefault();
+    var target= $(this.hash);
+    $('body,html').animate({
+      'scrollTop': target.offset().top + 1
+      }, 400
+    );
+  });
+
   // https://github.com/digitalBush/jquery.maskedinput
   $(".phone-mask").mask("+7(999) 999-9999");
 
@@ -29,6 +74,16 @@ $( document ).ready(function() {
     dots: false,
     loop: true,
     margin: 15,
-    center: true
+    center: true,
+    responsive : {
+      0:{
+        items: 1,
+        nav: false
+      },
+      480:{
+        items: 2,
+        nav: true
+      }
+    }
   });
 });
